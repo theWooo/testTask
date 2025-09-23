f91e8aeaf48f1bd9486b3d24f7c334f0b125d93a
theWooo <jxbox01@bk.ru>
2025-09-23 21:28:21 +0300

    Task2 is done

---
+++ Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System;
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator(-3, 26);
+            while (true) {
+                Console.WriteLine("Введите номер этажа: ");
+                int floor = Convert.ToInt32(Console.ReadLine());
+                elevator.move(floor);
+            }
+        }
+    }
+    class Elevator {
+        private int minFloor;
+        private int maxFloor;
+        private int currentFloor;
+        public Elevator(int minFloor, int maxFloor) {
+            this.minFloor = minFloor;
+            this.maxFloor = maxFloor;
+            currentFloor = 1;
+        }
+        public int getCurrentFloor() {
+            return currentFloor;
+        }
+        private void moveDown() {
+            if (currentFloor - 1 >= minFloor) {
+                currentFloor--;
+            }
+        }
+        private void moveUp() {
+            if (currentFloor + 1 <= maxFloor) {
+                currentFloor++;
+            }
+        }
+        public void move(int floor){
+            if (floor < minFloor || floor > maxFloor) {
+                Console.WriteLine($"Ошибка, указанный этаж {floor} не входит в диапазон доступных этажей.");
+                return;
+            }
+            Action direct;
+            if (floor < currentFloor) {
+                direct = moveDown;
+            }
+            else if (floor > currentFloor) {
+                direct = moveUp;
+            }
+            else { return; }
+            while (currentFloor != floor) {
+                direct();
+                Console.WriteLine(getCurrentFloor());
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+namespace Task2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Evaluator.getCircleArea(15));
+            Console.WriteLine(Evaluator.getCircleArea(5));
+            Console.WriteLine(Evaluator.getTriangleArea(5,5,5));
+            Console.WriteLine(Evaluator.getTriangleArea(6,7,12));
+            Console.WriteLine(Evaluator.getSphereVolume(5));
+            Console.WriteLine(Evaluator.getSphereVolume(1));
+            Console.WriteLine(Evaluator.isAPossibleTriangle(2, 3, 4));
+            Console.WriteLine(Evaluator.isAPossibleTriangle(2, 3, 6));
+        }
+    }
+    class Evaluator {
+        public static double getCircleArea(double radius) => Math.PI * Math.Pow(radius, 2);
+        public static double getTriangleArea(double a,double b, double c) {
+            if (!isAPossibleTriangle(a, b, c)) { throw new Exception("Невозможно построить треугольник с указанными сторонами"); }
+            double P = (a + b + c) / 2;
+            return Math.Sqrt(P*(P-a)*(P-b)*(P-c));
+        }
+        public static double getSphereVolume(double radius) => ((4 * Math.PI) / 3)  * Math.Pow(radius, 3);
+        public static bool isAPossibleTriangle(double a, double b, double c) {
+            double longest = new double[] { a, b, c }.OrderByDescending(it => it).First();
+            return longest - (a + b + c - longest) < 0;
+        }
+    }
+}
